06ee9e895355f74422d0d00e9c1845d430f40204
macbookair <nj.martin123@gmail.com>
2015-10-20 14:21:21 -0400

    Homework 3.2

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,46 @@
 package sample;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
-public class Controller {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    ObservableList<Contact> contacts = FXCollections.observableArrayList();
+
+    @FXML
+    TextField nameField;
+    @FXML
+    TextField emailField;
+    @FXML
+    TextField phoneField;
+    @FXML
+    ListView listView;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        listView.setItems(contacts);
+
+    }
+    public void addContact() {
+        Contact contact = new Contact(nameField.getText(), emailField.getText(), phoneField.getText());
+        if(!nameField.getText().equals("") && !emailField.getText().equals("")&&!phoneField.getText().equals("")){
+            contacts.add(contact);
+        }
+        nameField.setText("");
+        phoneField.setText("");
+        emailField.setText("");
+    }
+
+    public void removeContact(){
+        Contact contact = (Contact) listView.getSelectionModel().getSelectedItem();
+        if (contact != null) {
+            contacts.remove(contact);
+        }
+    }
 }
